df85d45cb4864fe93e3dfe26e49aaba6a65b735d
Matt-washere <Porphyrins@proton.me>
2025-07-18 14:56:07 -0500

    Fix TextEncoder polyfill for Jest test environment

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,10 @@
 // Jest setup file
 process.env.NODE_ENV = 'test';
 
+// Polyfill for TextEncoder (needed for some dependencies)
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
 // Increase timeout for async operations
 jest.setTimeout(10000); 
