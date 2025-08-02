73aa27b5927a0b4c5e6b6ff08006b1598feef874
MontyPithon <cnewosme490@gmail.com>
2025-08-02 18:17:28 -0500

    add role handling to registration and login

--- src/api/authValidation.js
+++ src/api/authValidation.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 
 exports.validateRegistration = (data) => Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required()
+  password: Joi.string().required(),
+  role: Joi.string().valid('volunteer', 'admin').default('volunteer')
 }).validate(data);
 
 exports.validateLogin = (data) => Joi.object({
